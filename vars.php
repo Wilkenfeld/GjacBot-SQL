dcd48e17344c7f0f45c29927d04b2853de30d5b3
GiacomoDevIT <45907453+GiacomoDevIT@users.noreply.github.com>
2020-07-13 17:26:01 +0200

    Update vars.php

--- vars.php
+++ vars.php
@@ -1,5 +1,22 @@
 <?php
 if(null !== update) {
+	$testo = update['message']['text']; 
+	$chatID = update["message"]["chat"]["id"];
+	$userID = update["message"]["from"]["id"];
+	$msgID = $update['message']['message_id'];
+	$username = update["message"]["from"]["username"];
+	$nome = update["message"]["from"]["first_name"];
+	$cognome = update["message"]["from"]["last_name"];
+	$tuttoilnome = $nome."".$cognome;
+	$location = update["message"]["location"];
+	$longitudine = update["message"]["location"]["longitude"];
+	$latitudine = update["message"]["location"]["latitude"];
+	$voice = update["message"]["voice"]["file_id"];
+	$photo = update["message"]["photo"][0]["file_id"];
+	$document = update["message"]["document"]["file_id"];
+	$audio = update["message"]["audio"]["file_id"];
+	$sticker = update["message"]["sticker"]["file_id"];
+	$lingua = update["message"]["from"]["language_code"];
 	if(update['callback_query']) {
 		$cbid = update['callback_query']['id'];
 		$cbdata = update['callback_query']['data'];
@@ -15,29 +32,5 @@ if(null !== update) {
 		$cbid = '';
 		$cbdata = '';
 		$cbmsg = '';
-		$msgID = '';
-		$chatID = '';
-		$userID = '';
-		$nome = '';
-		$cognome = '';
-		$username = '';
-		$lang = '';
 	}
-	$testo = update['message']['text']; 
-	$chatID = update["message"]["chat"]["id"];
-	$userID = update["message"]["from"]["id"];
-	$username = update["message"]["from"]["username"];
-	$nome = update["message"]["from"]["first_name"];
-	$cognome = update["message"]["from"]["last_name"];
-	$tuttoilnome = $nome."".$cognome;
-	$location = update["message"]["location"];
-	$longitudine = update["message"]["location"]["longitude"];
-	$latitudine = update["message"]["location"]["latitude"];
-	$voice = update["message"]["voice"]["file_id"];
-	$photo = update["message"]["photo"][0]["file_id"];
-	$document = update["message"]["document"]["file_id"];
-	$audio = update["message"]["audio"]["file_id"];
-	$sticker = update["message"]["sticker"]["file_id"];
-	$lingua = update["message"]["from"]["language_code"];
 }
-$isadmin = in_array($chatID, $admin); //da il true se la chat è admin
